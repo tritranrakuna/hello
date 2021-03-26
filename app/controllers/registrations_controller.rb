a55093bd67130ccaa30bbba2ec46ffbd5ee8a264
Tri Tran <tranminhtri888@gmail.com>
2021-03-26 11:55:49 +0700

    Heroku config

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,12 @@
+class RegistrationContrller < Devise::RegistrationContrller
+
+    private
+    
+    def sign_up_params
+        params.require(:user).permit(:name, :username, :email, :password, :password_comfirmation)
+    end
+
+    def account_update_params
+        params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :current_password)
+    end
+end
---
+++ app/models/tweeet.rb
@@ -0,0 +1,4 @@
+class Tweeet < ApplicationRecord
+    belongs_to :user
+    has_many :comments
+end
